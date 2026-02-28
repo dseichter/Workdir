b8bd5abee9a8120d43210d44839834ab2c3e80c0
Daniel Seichter <daniel.seichter@dseichter.de>
2026-02-28 21:37:14 +0100

    Correct copyright

--- src/gui.py
+++ src/gui.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 
-###########################################################################
-## Python code migrated from wxPython to PySide6
-## Original generated with wxFormBuilder (version 4.2.1-0-g80c4cb6)
+# Copyright (c) 2024-2026 Daniel Seichter
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+
 from PySide6.QtWidgets import QMainWindow, QWidget, QDialog, QColorDialog, QVBoxLayout, QGridLayout, QLabel, QLineEdit, QCheckBox, QPushButton, QTextEdit, QDialogButtonBox
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QAction, QColor
